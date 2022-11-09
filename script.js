a60257e852dd90d98fd9b5a30881748921f67227
Rafael Alberto Larios <albertolarios@me.com>
2022-11-09 08:17:51 -0600

    Add Gameboard object with Module patter

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("All set up");
+
+const Gameboard = (function() {
+    const _createBoard = function(rows, columns) {
+        let row = Array.apply(null, Array(columns));
+        let array2d = [];
+        for (let i = 0; i < columns; i++) {
+            array2d.push(row);
+        }
+        return array2d;
+    };
+    const board = _createBoard(3, 3);
+    return {board};
+})();
