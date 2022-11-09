4169b7776c003e975dbb53fc8d6e36f447a72ff9
Rafael Alberto Larios <albertolarios@me.com>
2022-11-09 14:54:34 -0600

    Create player factory function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 console.log("All set up");
 
 const Gameboard = (function() {
-    const _createBoard = function(rows, columns) {
-        let row = Array.apply(null, Array(columns));
-        let array2d = [];
-        for (let i = 0; i < columns; i++) {
-            array2d.push(row);
+    const fillArray = function() {
+        for (let i in board) {
+            for (let j in board[i]) {
+                board[i][j] = "o";
+            }
         }
+        return board;
+    };
+    const _createBoard = function(rows, columns) {
+        //let row = Array.apply(null, Array(columns));
+        let row =  [...Array(3)];
+        let array2d = Array(3).fill(row);
         return array2d;
     };
     const board = _createBoard(3, 3);
-    return {board};
+    return {board, fillArray};
 })();
+
+const player = function(name) {
+    let playerName = name;
+    return {playerName, extendName};
+};
